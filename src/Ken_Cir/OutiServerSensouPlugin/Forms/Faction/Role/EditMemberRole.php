9ac08ff0f20c2261c9fad6a604be67875fc14fd0
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2021-11-11 19:12:09 +0900

    closes #8, #10

--- src/Ken_Cir/OutiServerSensouPlugin/Forms/Faction/Role/EditMemberRole.php
+++ src/Ken_Cir/OutiServerSensouPlugin/Forms/Faction/Role/EditMemberRole.php
@@ -26,11 +26,9 @@ class EditMemberRole
 
     public function execute(Player $player): void
     {
-        // A var
         try {
             $player_data = PlayerDataManager::getInstance()->get($player->getName());
             $factionPlayers = array_values(PlayerDataManager::getInstance()->getFactionPlayers($player_data->getFaction()));
-            // A var
             $form = new SimpleForm(function (Player $player, $data) use ($factionPlayers) {
                 try {
                     if ($data === null) return true;
@@ -57,42 +55,8 @@ class EditMemberRole
         catch (Error | Exception $e) {
             Main::getInstance()->getPluginLogger()->error($e, $player);
         }
-
-        // B var
-        /*
-        try {
-            $player_data = PlayerDataManager::getInstance()->get($player->getName());
-            $factionRoles = array_values(RoleDataManager::getInstance()->getFactionRoles($player_data->getFaction()));
-            $form = new SimpleForm(function (Player $player, $data) use ($factionRoles) {
-                try {
-                    if ($data === null) return true;
-                    elseif ($data === 0) {
-                        $form = new RoleManagerForm();
-                        $form->execute($player);
-                        return true;
-                    }
-                    $this->edit_B($player, $factionRoles[$data - 1]);
-                }
-                catch (Error | Exception $e) {
-                    Main::getInstance()->getPluginLogger()->error($e, $player);
-                }
-
-                return true;
-            });
-            $form->setTitle("§b派閥役職所持メンバー操作フォーム");
-            $form->addButton("戻る");
-            foreach ($factionRoles as $factionRole) {
-                $form->addButton($factionRole->getName());
-            }
-            $player->sendForm($form);
-        }
-        catch (Error | Exception $e) {
-            Main::getInstance()->getPluginLogger()->error($e, $player);
-        }
-        */
     }
 
-    // A var
     private function edit_A(Player $player, PlayerData $editPlayerData): void
     {
         try {
@@ -135,57 +99,6 @@ class EditMemberRole
                 }
                 else {
                     $form->addToggle($role->getName());
-                }
-            }
-            $player->sendForm($form);
-        }
-        catch (Error | Exception $e) {
-            Main::getInstance()->getPluginLogger()->error($e, $player);
-        }
-    }
-
-    // B var
-    private function edit_B(Player $player, RoleData $editRoleData): void
-    {
-        try {
-            $factionPlayers = array_values(PlayerDataManager::getInstance()->getFactionPlayers($editRoleData->getFactionId()));
-            $form = new CustomForm(function (Player $player, $data) use ($editRoleData, $factionPlayers) {
-                try {
-                    if ($data === null) return true;
-                    elseif ($data[1] === true) {
-                        $this->execute($player);
-                        return true;
-                    }
-                    $msg = "[システム] {$editRoleData->getName()}のロール所持メンバーを以下の通りに変更しました\n";
-                    for ($i = 2; $i < count($data); $i++) {
-                        if ($data[$i] === true and !$factionPlayers[$i - 2]->hasRole($editRoleData->getId())) {
-                            $factionPlayers[$i - 1]->addRole($editRoleData->getId());
-                            $msg .= "{$factionPlayers[$i - 2]->getName()}に付与しました\n";
-                        }
-                        elseif ($data[$i] === false and $factionPlayers[$i - 2]->hasRole($editRoleData->getId())) {
-                            $factionPlayers[$i - 2]->removeRole($editRoleData->getId());
-                            $msg .= "{$factionPlayers[$i - 2]->getName()}から剥奪しました\n";
-                        }
-                    }
-
-                    $player->sendMessage($msg);
-                    Main::getInstance()->getScheduler()->scheduleDelayedTask(new ReturnForm([$this, "execute"], [$player]), 10);
-                }
-                catch (Error | Exception $e) {
-                    Main::getInstance()->getPluginLogger()->error($e, $player);
-                }
-
-                return true;
-            });
-            $form->setTitle("派閥役職所持メンバー操作フォーム {$editRoleData->getName()}");
-            $form->addLabel("メンバーを操作する役職名: {$editRoleData->getName()}");
-            $form->addToggle("キャンセルして戻る");
-            foreach ($factionPlayers as $factionPlayer) {
-                if ($factionPlayer->hasRole($editRoleData->getId())) {
-                    $form->addToggle($factionPlayer->getName(), true);
-                }
-                else {
-                    $form->addToggle($factionPlayer->getName());
                 }
             }
             $player->sendForm($form);
