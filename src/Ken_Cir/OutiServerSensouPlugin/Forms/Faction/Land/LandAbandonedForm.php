0e79d529c91a6787d93460b2d6f5f76ca795f232
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2021-12-21 18:45:48 +0900

    土地放棄追加

---
+++ src/Ken_Cir/OutiServerSensouPlugin/Forms/Faction/Land/LandAbandonedForm.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ken_Cir\OutiServerSensouPlugin\Forms\Faction\Land;
+
+use Error;
+use Exception;
+use Ken_Cir\OutiServerSensouPlugin\Main;
+use Ken_Cir\OutiServerSensouPlugin\Managers\LandData\LandDataManager;
+use pocketmine\player\Player;
+use Vecnavium\FormsUI\ModalForm;
+
+class LandAbandonedForm
+{
+    public function __construct()
+    {
+    }
+
+    public function execute(Player $player): void
+    {
+        try {
+            $form = new ModalForm(function(Player $player, $data) {
+                try {
+                    if ($data === true) {
+                        $landData = LandDataManager::getInstance()->getChunk((int)$player->getPosition()->getX() >> 4, (int)$player->getPosition()->getZ() >> 4, $player->getWorld()->getFolderName());
+                        LandDataManager::getInstance()->delete($landData->getId());
+                        $player->sendMessage("§a[システム] 削除しました");
+                    }
+                }
+                catch (Error | Exception $e) {
+                    Main::getInstance()->getPluginLogger()->error($e, $player);
+                }
+            });
+
+            $form->setTitle("土地放棄確認");
+            $form->setContent("現在立っているチャンクの土地を放棄してもよろしいですか？");
+            $form->setButton1("放棄");
+            $form->setButton2("キャンセル");
+            $player->sendForm($form);
+        }
+        catch (Error | Exception $e) {
+            Main::getInstance()->getPluginLogger()->error($e, $player);
+        }
+    }
+}
