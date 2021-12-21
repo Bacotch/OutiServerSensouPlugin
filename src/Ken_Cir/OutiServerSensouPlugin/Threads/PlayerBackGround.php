d3d4a1cc5d55b1811a95988efe3f9839e82069db
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2021-12-21 18:26:41 +0900

    スコアボード系のTaskを修正

--- src/Ken_Cir/OutiServerSensouPlugin/Threads/PlayerBackGround.php
+++ src/Ken_Cir/OutiServerSensouPlugin/Threads/PlayerBackGround.php
@@ -2,14 +2,24 @@
 
 namespace Ken_Cir\OutiServerSensouPlugin\Threads;
 
+use Error;
+use Exception;
+use Ken_Cir\OutiServerSensouPlugin\Main;
+use Ken_Cir\OutiServerSensouPlugin\Managers\FactionData\FactionDataManager;
+use Ken_Cir\OutiServerSensouPlugin\Managers\LandData\LandDataManager;
+use Ken_Cir\OutiServerSensouPlugin\Managers\PlayerData\PlayerDataManager;
 use pocketmine\item\ItemFactory;
+use pocketmine\network\mcpe\protocol\RemoveObjectivePacket;
+use pocketmine\network\mcpe\protocol\SetDisplayObjectivePacket;
+use pocketmine\network\mcpe\protocol\SetScorePacket;
+use pocketmine\network\mcpe\protocol\types\ScorePacketEntry;
+use pocketmine\player\Player;
 use pocketmine\scheduler\Task;
 use pocketmine\Server;
 
 /**
  * プレイヤー裏処理タスク
- * おうちウォッチ付与・不正確認など
- * 基本表には出ない別にラグが出ても対して問題のないもの
+ * おうちウォッチ付与・不正確認・スコアボードなど
  */
 class PlayerBackGround extends Task
 {
@@ -19,8 +29,37 @@ class PlayerBackGround extends Task
 
     public function onRun(): void
     {
-        // ---サーバーにいるプレイヤーにおうちウォッチがなかったら付与する
+        // ---サーバーにいるプレイヤーを全員取得---
         foreach (Server::getInstance()->getOnlinePlayers() as $player) {
+            // ---スコアボードの描写---
+            $player_data = PlayerDataManager::getInstance()->get($player->getName());
+            $factionLandData = LandDataManager::getInstance()->getChunk((int)$player->getPosition()->getX() >> 4, (int)$player->getPosition()->getZ() >> 4, $player->getWorld()->getFolderName());
+            if ($player_data) {
+                $this->RemoveData($player);
+                if ($player_data->getDrawscoreboard() === 0) continue;
+                $this->setupData($player);
+                $this->sendData($player, "§b座標: " . $player->getPosition()->getFloorX() . "," . $player->getPosition()->getFloorY() . "," . $player->getPosition()->getFloorZ(), 1);
+                $this->sendData($player, "§bワールド: " . $player->getWorld()->getFolderName(), 2);
+                $this->sendData($player, "§c現在時刻: " . date("G時i分s秒"), 3);
+                $this->sendData($player, "§6持ってるアイテムid: " . $player->getInventory()->getItemInHand()->getId() . ":" . $player->getInventory()->getItemInHand()->getMeta(), 4);
+                $this->sendData($player, "§dPing: " . $player->getNetworkSession()->getPing() . "ms", 5);
+                if ($player_data->getFaction() === -1) {
+                    $this->sendData($player, "§a所属派閥: 無所属", 6);
+                }
+                else {
+                    $faction_data = FactionDataManager::getInstance()->get($player_data->getFaction());
+                    $this->sendData($player, "§a所属派閥: {$faction_data->getName()}", 6);
+                }
+                if (!$factionLandData) {
+                    $this->sendData($player, "現在立っているチャンクはどの派閥も所有していません", 7);
+                }
+                else {
+                    $landFaction = FactionDataManager::getInstance()->get($factionLandData->getFactionId());
+                    $this->sendData($player, "現在立っているチャンクは派閥 {$landFaction->getName()}が所有しています", 7);
+                }
+            }
+
+            // ---おうちウォッチを持っていなかったら渡す
             $item = ItemFactory::getInstance()->get(347);
             $item->setCustomName("OutiWatch");
             if (!$player->getInventory()->contains($item)) {
@@ -28,4 +67,54 @@ class PlayerBackGround extends Task
             }
         }
     }
+
+    // ここから
+    // ---スコアボード系用の処理---
+    private function setupData(Player $player)
+    {
+        try {
+            $pk = new SetDisplayObjectivePacket();
+            $pk->displaySlot = "sidebar";
+            $pk->objectiveName = "sidebar";
+            $pk->displayName = "§a" . $player->getName();
+            $pk->criteriaName = "dummy";
+            $pk->sortOrder = 0;
+            $player->getNetworkSession()->sendDataPacket($pk);
+        }
+        catch (Error | Exception $error) {
+            Main::getInstance()->getPluginLogger()->error($error);
+        }
+    }
+
+    private function sendData(Player $player, string $data, int $id)
+    {
+        try {
+            $entry = new ScorePacketEntry();
+            $entry->objectiveName = "sidebar";
+            $entry->type = $entry::TYPE_FAKE_PLAYER;
+            $entry->customName = $data;
+            $entry->score = $id;
+            $entry->scoreboardId = $id + 11;
+            $pk = new SetScorePacket();
+            $pk->type = $pk::TYPE_CHANGE;
+            $pk->entries[] = $entry;
+            $player->getNetworkSession()->sendDataPacket($pk);
+        }
+        catch (Error | Exception $error) {
+            Main::getInstance()->getPluginLogger()->error($error);
+        }
+    }
+
+    private function RemoveData(Player $player)
+    {
+        try {
+            $pk = new RemoveObjectivePacket();
+            $pk->objectiveName = "sidebar";
+            $player->getNetworkSession()->sendDataPacket($pk);
+        }
+        catch (Error | Exception $error) {
+            Main::getInstance()->getPluginLogger()->error($error);
+        }
+    }
+    // ここまで
 }
