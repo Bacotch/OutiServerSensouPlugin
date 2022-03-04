dfdea763bed5e22805ea1cbd84ef25912ba3a824
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-03-05 02:38:41 +0900

    レポート・要望フォームを送信するように修正

--- src/ken_cir/outiserversensouplugin/forms/ReportForm.php
+++ src/ken_cir/outiserversensouplugin/forms/ReportForm.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace ken_cir\outiserversensouplugin\forms;
 
-
+use CortexPE\DiscordWebhookAPI\Message;
+use CortexPE\DiscordWebhookAPI\Webhook;
 use jojoe77777\FormAPI\CustomForm;
 use ken_cir\outiserversensouplugin\Main;
 use ken_cir\outiserversensouplugin\tasks\ReturnForm;
@@ -29,11 +30,24 @@ class ReportForm
             $form = new CustomForm(function (Player $player, $data) {
                 try {
                     if ($data === null) return true;
-                    elseif (!isset($data[0], $data[1])) {
+                    elseif ($data[0]) {
+                        (new OutiWatchForm())->execute($player);
+                    }
+                    elseif (!$data[1] or !$data[2]) {
                         $player->sendMessage("§a[システム] プレイヤー名と内容部分を空にすることはできません");
-                        Main::getInstance()->getScheduler()->scheduleDelayedTask(new ReturnForm([$this, "execute"], [$player]), 10);
-                    } else {
-                        $player->sendMessage("§a[システム] レポートを送信しました");
+                        Main::getInstance()->getScheduler()->scheduleDelayedTask(new ReturnForm([$this, "execute"], [$player]), 20);
+                    }
+                    else {
+                        if (($webhookURL = (string)Main::getInstance()->getPluginConfig()->get("Report_Request_Webhook", "")) !== "") {
+                            $webhook = new Webhook($webhookURL);
+                            $msg = new Message();
+                            $msg->setContent("**レポート**\n\n{$player->getName()}(XUID: {$player->getXuid()})のレポート\n```\nレポート対象のプレイヤー名: $data[1]\nレポート内容: $data[2]```");
+                            $webhook->send($msg);
+                            $player->sendMessage("§a[システム] レポートを送信しました");
+                        }
+                        else {
+                            $player->sendMessage("§a[システム] レポートを送信できませんでした、レポート先のWebhookURLが設定されていません");
+                        }
                     }
                 } catch (\Error|\Exception $e) {
                     Main::getInstance()->getOutiServerLogger()->error($e, true, $player);
@@ -43,6 +57,7 @@ class ReportForm
             });
 
             $form->setTitle("レポートフォーム");
+            $form->addToggle("キャンセルして戻る");
             $form->addInput("§cレポートするプレイヤー名", "player_name");
             $form->addInput("§d内容", "content");
             $form->addLabel("§e[注意] 嘘のレポートは処罰される可能性があります\n処罰等が決定次第、内部メールで連絡致します");
--- src/ken_cir/outiserversensouplugin/forms/RequestForm.php
+++ src/ken_cir/outiserversensouplugin/forms/RequestForm.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace ken_cir\outiserversensouplugin\forms;
 
 
+use CortexPE\DiscordWebhookAPI\Message;
+use CortexPE\DiscordWebhookAPI\Webhook;
 use jojoe77777\FormAPI\CustomForm;
 use ken_cir\outiserversensouplugin\Main;
 use ken_cir\outiserversensouplugin\tasks\ReturnForm;
@@ -29,11 +31,23 @@ class RequestForm
             $form = new CustomForm(function (Player $player, $data) {
                 try {
                     if ($data === null) return true;
-                    elseif (!isset($data[0])) {
+                    elseif ($data[0]) {
+                        (new OutiWatchForm())->execute($player);
+                    }
+                    elseif (!$data[1]) {
                         $player->sendMessage("§a[システム] 要望部分を空にすることはできません");
-                        Main::getInstance()->getScheduler()->scheduleDelayedTask(new ReturnForm([$this, "execute"], [$player]), 10);
+                        Main::getInstance()->getScheduler()->scheduleDelayedTask(new ReturnForm([$this, "execute"], [$player]), 20);
                     } else {
-                        $player->sendMessage("§a[システム] 要望を送信しました");
+                        if (($webhookURL = (string)Main::getInstance()->getPluginConfig()->get("Report_Request_Webhook", "")) !== "") {
+                            $webhook = new Webhook($webhookURL);
+                            $msg = new Message();
+                            $msg->setContent("**要望**\n\n{$player->getName()}(XUID: {$player->getXuid()})の要望\n```\n要望内容: $data[1]```");
+                            $webhook->send($msg);
+                            $player->sendMessage("§a[システム] 要望を送信しました");
+                        }
+                        else {
+                            $player->sendMessage("§a[システム] 要望を送信できませんでした、要望先のWebhookURLが設定されていません");
+                        }
                     }
                 } catch (\Error|\Exception $e) {
                     Main::getInstance()->getOutiServerLogger()->error($e, true, $player);
@@ -43,6 +57,7 @@ class RequestForm
             });
 
             $form->setTitle("要望フォーム");
+            $form->addToggle("キャンセルして戻る");
             $form->addInput("§d内容", "content");
             $form->addLabel("§e要望内容に対する返信は内部メールで行います");
             $player->sendForm($form);
